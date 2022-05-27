6f182a440929becfa3c7c83de8d404a1102d597f
Eric M Stevens <36384238+ericmatthewstevens@users.noreply.github.com>
2022-05-26 22:35:25 -0500

    Revert "create basic login/signup pages"

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return render_template("login.html")
+    return "Login"
 
 @auth.route("/sign-up")
 def sign_up():
-    return render_template("signup.html")
+    return "Sign Up"
 
 @auth.route("/logout")
 def logout():
-    return redirect(url_for("views.home"))
+    return "Logout"
