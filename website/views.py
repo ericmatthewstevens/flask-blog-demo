a7810f6619af4a2d0b751aec6777c9bdd6cb0493
ericmatthewstevens <ericmatthew.stevens@gmail.com>
2022-05-26 22:34:17 -0500

    create basic login/signup pages

--- website/views.py
+++ website/views.py
@@ -5,4 +5,4 @@ views = Blueprint("views", __name__)
 @views.route("/home")
 @views.route("/")
 def home():
-    return render_template("home.html", name="Flask") #Flask actively looks for a template. Therefore, you must specify which template for flask to use
+    return render_template("home.html", name="Matt") #Flask actively looks for a template. Therefore, you must specify which template for flask to use
