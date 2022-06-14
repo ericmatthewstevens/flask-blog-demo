31530b54243c0e9c50e1edab2e7f1e97a016ff0d
ericmatthewstevens <ericmatthew.stevens@gmail.com>
2022-06-13 21:49:48 -0500

    User posts are visible on home page

--- website/models.py
+++ website/models.py
@@ -9,3 +9,15 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+        # posts serves as the relationship between the user and the user's posts. backref allows for access by calling the user, rather than the author from the Posts object.
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    ## author = db.Column(db.Integer)
+        ## This technically can work to identify the author of the post, but the database must be able to verify the user's existence in the db.
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+        ## ondelete='CASCADE' will delete any posts that are associated with the specified user.
+    
--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from crypt import methods
+from unicodedata import category
+from flask import Blueprint, render_template, request, flash
+from flask_login import current_user, login_required
+from .models import Post
+from . import db
 
 views = Blueprint("views", __name__)
 
@@ -7,4 +11,22 @@ views = Blueprint("views", __name__)
 @views.route("/")
 @login_required
 def home():
-    return render_template("home.html", name="Matt") #Flask actively looks for a template. Therefore, you must specify which template for flask to use
+    posts = Post.query.all()
+    return render_template("home.html", user=current_user, posts=posts) #Flask actively looks for a template. Therefore, you must specify which template for flask to use
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == "POST":
+        text = request.form.get('text')
+
+        if not text:
+            flash('Post cannot be empty', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template('create_posts.html', user=current_user)
