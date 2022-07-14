b1a2af762610fdac669fec9662b971f95c8a64f7
ericmatthewstevens <ericmatthew.stevens@gmail.com>
2022-07-14 15:44:47 -0500

    Comments can now be added to a blog post for each user

--- website/models.py
+++ website/models.py
@@ -1,4 +1,3 @@
-from time import timezone
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -11,6 +10,7 @@ class User(db.Model, UserMixin):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
         # posts serves as the relationship between the user and the user's posts. backref allows for access by calling the user, rather than the author from the Posts object.
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -20,4 +20,12 @@ class Post(db.Model):
         ## This technically can work to identify the author of the post, but the database must be able to verify the user's existence in the db.
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
         ## ondelete='CASCADE' will delete any posts that are associated with the specified user.
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    text = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)
     
